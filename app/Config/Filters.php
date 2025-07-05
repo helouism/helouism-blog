5ccf5cf10ce492ed332366e49479d61342ddbf1b
hendriklouis <hendrikmahdi@gmail.com>
2025-07-05 19:12:20 +0700

    update session except in globals filter

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -84,7 +84,7 @@ class Filters extends BaseFilters
                 ]
             ],
             'invalidchars',
-            'session' => ['except' => ['search', 'archive', 'archive/*', 'post/*', 'category-list', 'category/*', 'login*', '/', 'auth/a/*', 'logout', 'about']],
+            'session' => ['except' => ['*/login*', '*/register', '*/auth/a/*', '*/logout']],
 
         ],
         'after' => [
