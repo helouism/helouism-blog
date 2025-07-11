af186645c50dc325f7cfe959143323f931d32bd5
Hendrik Louis Mahdi <hendrikmahdi@gmail.com>
2025-07-11 22:38:27 +0700

    Update Controllers/Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -15,11 +15,6 @@ class Home extends BaseController
         $this->postModel = new PostModel();
         $this->categoryModel = new CategoryModel();
     }
-
-
-
-
-
     public function index(): string
     {
         $data = [
